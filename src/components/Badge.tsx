0ca36e4ebdba249fdaa94c81e651be9a0816375d
grand-ios <deograciousngereza@gmail.com>
2025-02-12 01:01:50 +0300

    Brand Updates to GMTECH

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,14 +51,14 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://gmtech.co.tz'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'GMTech Consult LTD'}</span>
           </a>
         </span>
       </Show>
